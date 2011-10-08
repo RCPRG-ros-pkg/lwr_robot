f3fa152441d5830d60d4cbbcda03300f50477688
Konrad Banachowicz <konradb3@gmail.com>
2011-10-08 15:47:28 +0200

    Electric compatibility fix.

--- lwr_bringup/simple_trajectory.py
+++ lwr_bringup/simple_trajectory.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
   goal = JointTrajectoryGoal()
   goal.trajectory.joint_names = ['lwr_arm_0_joint', 'lwr_arm_1_joint', 'lwr_arm_2_joint', 'lwr_arm_3_joint', 'lwr_arm_4_joint', 'lwr_arm_5_joint', 'lwr_arm_6_joint']
-  goal.trajectory.points.append(JointTrajectoryPoint([0.0, 0.5, 0.0, -0.5, -0.0, 0.0, 0.0], [0, 0, 0, 0, 0, 0, 0], [], rospy.Duration(5.0)))
+  goal.trajectory.points.append(JointTrajectoryPoint([1.825164385348162, 1.803825933603007, 0.000000000000000, 2.008362700195485, 1.321511416992363, -1.519663789792947, -1.768925223209182], [0, 0, 0, 0, 0, 0, 0], [], rospy.Duration(5.0)))
 
   goal.trajectory.header.stamp = rospy.get_rostime() + rospy.Duration(0.1)
 
